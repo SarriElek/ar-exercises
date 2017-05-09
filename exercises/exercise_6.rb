6308c6b1fc1c7212a45067148cb0f61d7db9889f
Edurne Berastegi Zabalza <edurne.berastegi@gmail.com>
2017-05-09 21:24:35 +0000

    Seventh exercise

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -13,12 +13,12 @@ puts "----------"
 
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 @store1.employees.create(first_name: "Edurne", last_name: "Berastegi", hourly_rate: 80)
-@store1.employees.create(first_name: "Javier", last_name: "Kimball", hourly_rate: 30)
+@store1.employees.create(first_name: "Javier", last_name: "Kimball", hourly_rate: 40)
 @store1.employees.create(first_name: "Anne", last_name: "Berastegi", hourly_rate: 60)
 @store1.employees.create(first_name: "Nikole", last_name: "Berastegi", hourly_rate: 60)
 
-@store2.employees.create(first_name: "Elvis", last_name: "Chan", hourly_rate: 10)
-@store2.employees.create(first_name: "Charles", last_name: "Chou", hourly_rate: 20)
+@store2.employees.create(first_name: "Elvis", last_name: "Chan", hourly_rate: 40)
+@store2.employees.create(first_name: "Charles", last_name: "Chou", hourly_rate: 40)
 @store2.employees.create(first_name: "Alvin", last_name: "NG", hourly_rate: 60)
 @store2.employees.create(first_name: "John", last_name: "Burks", hourly_rate: 60)
-@store2.employees.create(first_name: "Nikola", last_name: "Tesla", hourly_rate: 20)
+@store2.employees.create(first_name: "Nikola", last_name: "Tesla", hourly_rate: 40)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,19 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+
+
+# 2. Ask the user for a store name (store it in a variable)
+# 3. Attempt to create a store with the inputted name but leave out
+# the other fields (annual_revenue, mens_apparel, and womens_apparel)
+# 4. Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+
+store_name = gets.chomp
+store = Store.create(name:store_name)
+
+store.valid?
+
+store.errors.messages.each do |error|
+  puts "#{error[0]} #{error[1]}"
+end
+
