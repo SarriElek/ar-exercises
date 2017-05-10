c8ad60252fff026b6d8bc5ab365f7eeb1fb5b3c1
Edurne Berastegi Zabalza <edurne.berastegi@gmail.com>
2017-05-10 01:16:12 +0000

    9 exercise

--- lib/store.rb
+++ lib/store.rb
@@ -5,10 +5,20 @@ class Store < ActiveRecord::Base
   validates :annual_revenue, numericality: { only_integer: true,  greater_than_or_equal_to: 0}
   validate :should_have_mens_or_womens_apparel
 
+  before_destroy :check_employee_number
+
+  private
 
   def should_have_mens_or_womens_apparel
     if !mens_apparel && !womens_apparel
       errors.add(:base, "the store must carry at least one of the men's or women's apparel")
+    end
+  end
+
+  def check_employee_number
+    if employees.any?
+        errors.add(:base, "You can't delete a store with employees")
+        throw :abort
     end
   end
 
